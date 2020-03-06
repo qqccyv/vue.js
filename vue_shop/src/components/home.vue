d07bf6a7d939a17c7c67f005aaec6ea478ea03b6
dengyu <313187515@qq.com>
2020-03-06 20:16:48 +0800

    登录拦截和退出功能完善

--- vue_shop/src/components/home.vue
+++ vue_shop/src/components/home.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    首页
+    <el-button type="primary" @click="loginOut">退出</el-button>
   </div>
 </template>
 
 <script>
 export default {
-  
+  methods: {
+    loginOut(){
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  },
 }
 </script>
 
