c4fa87f2ab05afe46552acde6465b4d888028fe3
dengyu <313187515@qq.com>
2020-03-07 17:06:04 +0800

    主页静态页面

--- vue_shop/src/components/home.vue
+++ vue_shop/src/components/home.vue
@@ -1,7 +1,32 @@
 <template>
-  <div>
+<el-container class="home-container">
+  <el-header>
+    <div>
+      <img src="../assets/heima.png" alt="">
+      <span>电商后台信息管理系统</span>
+    </div>
     <el-button type="primary" @click="loginOut">退出</el-button>
-  </div>
+    </el-header>
+  <el-container>
+    <el-aside width="200px">
+        <el-menu background-color="#545c64" text-color="#fff" active-text-color="#ffd04b">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-location"></i>
+            <span>导航一</span>
+          </template>
+            <el-menu-item index="1-1">
+                <i class="el-icon-location"></i>
+                <span>导航一</span>
+            </el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+
+
 </template>
 
 <script>
@@ -16,5 +41,48 @@ export default {
 </script>
 
 <style lang="less" scoped>
-  
+  .home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373d41;
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #fff;
+  font-size: 20px;
+  > div {
+    display: flex;
+    align-items: center;
+    span {
+      margin-left: 15px;
+    }
+  }
+}
+
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    border-right: none;
+  }
+}
+
+.el-main {
+  background-color: #eaedf1;
+}
+
+.iconfont {
+  margin-right: 10px;
+}
+
+.toggle-button {
+  background-color: #4a5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
