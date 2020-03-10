028839fedb3d02e6c09e3ab43d464c2f833f41d2
dengyu <313187515@qq.com>
2020-03-10 21:34:40 +0800

    将分配权限树状图表单显示，并将原有权限信息展示

--- vue_shop/src/components/power/roles.vue
+++ vue_shop/src/components/power/roles.vue
@@ -43,16 +43,25 @@
         <el-table-column type="index" label="#"></el-table-column>
         <el-table-column prop="roleName" label="角色名称"></el-table-column>
         <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
-        <!-- 状态栏 -->
+        <!-- 操作栏 -->
         <el-table-column label="操作" width="300px">
           <template slot-scope="rolesData">
             <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
             <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
-            <el-button type="warning" icon="el-icon-setting" size="mini">分配权限</el-button>
+            <el-button type="warning" icon="el-icon-setting" size="mini" @click="showAllotDialog(rolesData.row)">分配权限</el-button>
           </template>
         </el-table-column>
       </el-table>
     </el-card>
+        <!-- 分配权限弹出框 -->
+    <el-dialog title="分配权限" :visible.sync="allotDialogVisible" width="50%">
+      <!-- 树状图 -->
+      <el-tree :data="rightsList" :props="defaultProps" show-checkbox default-expand-all node-key="id" :default-checked-keys='defaultRightsList'></el-tree>
+      <span slot="footer">
+        <el-button @click="allotDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="allotDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
@@ -60,14 +69,22 @@
 export default {
   data() {
     return {
-      rolesList: []
+      rolesList: [],
+      rightsList: [],
+      defaultRightsList: [],
+      allotDialogVisible: false,
+      // 树状图，props选项可以接受一个对象，节点标题，和目标对象作为层级属性的名称
+      defaultProps: {
+        label:'authName',
+        children: 'children'
+      }
     }
   },
   created() {
     this.getRolesList()
   },
   methods: {
-    //获取权限列表
+    //获取角色列表
     async getRolesList() {
       let { data: res } = await this.$http.get('/roles')
       if (res.meta.status != 200) return this.$message({
@@ -76,29 +93,54 @@ export default {
         showClose: true,
       });
       this.rolesList = res.data
+      console.log(this.rolesList);
+      
     },
-   async deleteRights(role,id){
-           //删除确认
-    let result = await this.$confirm('此操作将永久删除该权限, 是否继续?', '提示', {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning'
-        }).catch(err=>err)
-        if(result != 'confirm') return
-      const {data:res}= await this.$http.delete(`roles/${role.id}/rights/${id}`)
-      if(res.meta.status != 200) return this.$message({
+    async deleteRights(role, id) {
+      //删除确认
+      let result = await this.$confirm('此操作将永久删除该权限, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).catch(err => err)
+      if (result != 'confirm') return
+      const { data: res } = await this.$http.delete(`roles/${role.id}/rights/${id}`)
+      if (res.meta.status != 200) return this.$message({
         message: res.meta.msg,
         type: 'error',
         showClose: true,
       });
       //通过删除请求返回的最新数据，直接替换角色的权限列表，防止刷新页面
       role.children = res.data
-       this.$message({
+      this.$message({
         message: res.meta.msg,
         type: 'success',
         showClose: true,
       });
-      
+
+    },
+    // 显示分配权限弹出框
+    async showAllotDialog(role){
+      // 获取角色的所有权限列表，并展示出来！！不只是当前角色的
+    let {data: res} = await this.$http.get('/rights/tree')
+    if(res.meta.status !=200) return this.$message({
+      message: res.meta.msg,
+      type: 'error',
+      showClose: true,
+    });
+      this.rightsList = res.data
+      //利用递归获取当前角色的三级权限id
+      this.queryRights(role,this.defaultRightsList)
+      this.allotDialogVisible = true
+    },
+    //定义获取三级权限的递归函数
+    queryRights(data,arr){
+      if(!data.children){
+        return arr.push(data.id)
+      }
+      data.children.forEach(item=>{
+        this.queryRights(item,arr)
+      })
     }
   },
 }
