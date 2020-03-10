70b295bf7c3a43995897751f5c8cf7f5e8b6d039
dengyu <313187515@qq.com>
2020-03-10 18:59:47 +0800

    添加删除权限功能

--- vue_shop/src/components/power/roles.vue
+++ vue_shop/src/components/power/roles.vue
@@ -21,19 +21,19 @@
             <!-- 一级权限 -->
             <el-row v-for="(item1, index1) in scope.row.children" :key="item1.id" :class="{bdbottom:true,bdtop: index1 == 0,vcenter:true}">
               <el-col :span="5">
-                <el-tag>{{item1.authName}}</el-tag>
+                <el-tag closable @close="deleteRights(scope.row,item1.id)">{{item1.authName}}</el-tag>
                 <i class="el-icon-caret-right"></i>
               </el-col>
               <el-col :span="19">
                 <!-- 二级权限 -->
                 <el-row v-for="(item2, index2) in item1.children" :key="item2.id" :class="{bdtop: index2 != 0,vcenter:true}">
                   <el-col :span="6">
-                    <el-tag type="success">{{item1.authName}}</el-tag>
+                    <el-tag type="success" closable @close="deleteRights(scope.row,item2.id)">{{item1.authName}}</el-tag>
                     <i class="el-icon-caret-right"></i>
                   </el-col>
                   <el-col :span="18">
                     <!-- 三级权限 -->
-                    <el-tag type="warning" v-for="(item3, index3) in item2.children" :key="item3.id">{{item3.authName}}</el-tag>
+                    <el-tag type="warning" v-for="(item3, index3) in item2.children" :key="item3.id" closable @close="deleteRights(scope.row,item3.id)">{{item3.authName}}</el-tag>
                   </el-col>
                 </el-row>
               </el-col>
@@ -67,6 +67,7 @@ export default {
     this.getRolesList()
   },
   methods: {
+    //获取权限列表
     async getRolesList() {
       let { data: res } = await this.$http.get('/roles')
       if (res.meta.status != 200) return this.$message({
@@ -75,6 +76,29 @@ export default {
         showClose: true,
       });
       this.rolesList = res.data
+    },
+   async deleteRights(role,id){
+           //删除确认
+    let result = await this.$confirm('此操作将永久删除该权限, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err=>err)
+        if(result != 'confirm') return
+      const {data:res}= await this.$http.delete(`roles/${role.id}/rights/${id}`)
+      if(res.meta.status != 200) return this.$message({
+        message: res.meta.msg,
+        type: 'error',
+        showClose: true,
+      });
+      //通过删除请求返回的最新数据，直接替换角色的权限列表，防止刷新页面
+      role.children = res.data
+       this.$message({
+        message: res.meta.msg,
+        type: 'success',
+        showClose: true,
+      });
+      
     }
   },
 }
