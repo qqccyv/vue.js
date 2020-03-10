8a3397061073ef5ed0af1b90a93589fe87a3d6a0
dengyu <313187515@qq.com>
2020-03-10 17:49:40 +0800

    角色权限列表渲染

--- vue_shop/src/components/power/roles.vue
+++ vue_shop/src/components/power/roles.vue
@@ -15,7 +15,31 @@
       </el-row>
       <!-- 角色列表 -->
       <el-table :data="rolesList" border stripe>
-        <el-table-column type="expand"></el-table-column>
+        <el-table-column type="expand">
+          <!-- 角色权限列表 -->
+          <template slot-scope="scope">
+            <!-- 一级权限 -->
+            <el-row v-for="(item1, index1) in scope.row.children" :key="item1.id" :class="{bdbottom:true,bdtop: index1 == 0,vcenter:true}">
+              <el-col :span="5">
+                <el-tag>{{item1.authName}}</el-tag>
+                <i class="el-icon-caret-right"></i>
+              </el-col>
+              <el-col :span="19">
+                <!-- 二级权限 -->
+                <el-row v-for="(item2, index2) in item1.children" :key="item2.id" :class="{bdtop: index2 != 0,vcenter:true}">
+                  <el-col :span="6">
+                    <el-tag type="success">{{item1.authName}}</el-tag>
+                    <i class="el-icon-caret-right"></i>
+                  </el-col>
+                  <el-col :span="18">
+                    <!-- 三级权限 -->
+                    <el-tag type="warning" v-for="(item3, index3) in item2.children" :key="item3.id">{{item3.authName}}</el-tag>
+                  </el-col>
+                </el-row>
+              </el-col>
+            </el-row>
+          </template>
+        </el-table-column>
         <el-table-column type="index" label="#"></el-table-column>
         <el-table-column prop="roleName" label="角色名称"></el-table-column>
         <el-table-column prop="roleDesc" label="角色描述"></el-table-column>
@@ -57,4 +81,20 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-tag {
+  margin: 7px;
+}
+
+.bdtop {
+  border-top: 1px solid #eee;
+}
+
+.bdbottom {
+  border-bottom: 1px solid #eee;
+}
+
+.vcenter {
+  display: flex;
+  align-items: center;
+}
 </style>
