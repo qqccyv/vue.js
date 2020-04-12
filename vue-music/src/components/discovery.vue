a877f57194d53fb275d254c75ccaef9159a8fc63
dengyu <313187515@qq.com>
2020-04-12 18:25:44 +0800

    跨域问题解决不了，慢慢研究

--- vue-music/src/components/discovery.vue
+++ vue-music/src/components/discovery.vue
@@ -6,7 +6,21 @@
 
 <script>
 export default {
+  data () {
+    return {
 
+    }
+  },
+  created () {
+    // 获取轮播图数据
+    this.getBanner()
+  },
+  methods: {
+    async getBanner () {
+      const res = await this.$http.post('v2/banner/get', { clientType: 'pc' })
+      console.log(res)
+    }
+  }
 }
 </script>
 
--- vue-music/src/main.js
+++ vue-music/src/main.js
@@ -2,11 +2,25 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
+import qs from 'qs'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import './assets/css/global.css'
 Vue.use(ElementUI)
-
+Axios.defaults.baseURL = 'http://autumnfish.cn/api/'
+Axios.defaults.withCredentials = true
+Axios.defaults.crossDomain = true
+Axios.interceptors.request.use(config => {
+  config.headers['Content-Type'] = 'application/x-www-form-urlencoded'
+  if (config.method === 'post') {
+    config.data = qs.stringify({
+      ...config.data
+    })
+  }
+  return config
+})
+Vue.prototype.$http = Axios
 Vue.config.productionTip = false
 
 new Vue({
