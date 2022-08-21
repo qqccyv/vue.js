936e8425cc451b9b2feba2426c786b814cd3520b
dengyu <313187515@qq.com>
2022-08-21 18:14:14 +0800

    ts 文字类型

--- vue3-test/src/App.vue
+++ vue3-test/src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
-import { usePoint, IPoint } from "./hooks/usePoint.ts";
+import { onMounted } from "vue";
+import { usePoint, IPoint } from "./hooks/usePoint";
 const point: IPoint = usePoint();
+onMounted(() => {
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d");
+  const obj = {
+    count: 1,
+  } as const; // 将类型固定后属性值才能被用于文字类型的变量，因为obj.count 可以是任何值
+  function test(params: 1) {
+    console.log(params);
+  }
+  test(obj.count);
+});
 </script>
 
 <template>
-  <h2>{{point.x}}<br />{{point.y}}</h2>
+  <h2>{{ point.x }}<br />{{ point.y }}</h2>
+  <canvas id="canvas"></canvas>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
